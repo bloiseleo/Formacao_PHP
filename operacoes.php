a49cddfb849e1b3d1c838286183986af9faca291
Leonardo C. S. Bloise <leonardocsbloise@gmail.com>
2022-11-25 11:32:34 -0300

    feat: operacoes em php

---
+++ operacoes.php
@@ -0,0 +1,14 @@
+<?php
+
+$idade = 22;
+
+$idadeDaquiDezAnos = $idade + 10;
+$idadeDaquiCincoAnos = $idadeDaquiDezAnos - 5;
+$metadeDaIdade = $idade/2;
+$idadeAoQuadrado = $idade ** 2;
+$restoDaDivisao = $idade % 3;
+
+
+echo "Eu tenho $idade e, daqui a 10 anos, terei $idadeDaquiDezAnos. Daqui a 5 anos, terei $idadeDaquiCincoAnos. A metade da minha idade é $metadeDaIdade".PHP_EOL;
+echo "Minha idade ao quadrado é igual a $idadeAoQuadrado".PHP_EOL;
+echo "O resto da divisão por 3 é $restoDaDivisao".PHP_EOL;
