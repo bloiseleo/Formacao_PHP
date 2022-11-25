cb1fd099b9ab9a867aa75477ba23fe2cb6b23f6e
Leonardo C. S. Bloise <leonardocsbloise@gmail.com>
2022-11-25 12:00:25 -0300

    feat: working with types

---
+++ tipos.php
@@ -0,0 +1,11 @@
+<?php
+
+$idade = 21;
+$salario = 1000.30;
+$resultado = 10 / 3;
+$nome = "Leonardo";
+$salarioMaiorQue1000 = $salario > 1000;
+
+if($salarioMaiorQue1000) {
+    echo "Maior que 1000";
+}
